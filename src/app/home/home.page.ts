07288256efabcc60d8d4598c022d8101e64618f3
Nur Ridho Abdurrahmansyah <abdurrahmansyah234@gmail.com>
2019-09-11 18:38:02 +0700

    - Modify popover - Masukin kelas data date

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,9 +8,11 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
 
-  datas: any = [];
+  timeDatas: any = [];
   geoLatitude: number;
   geoLongitude: number;
   lat: number;
@@ -19,86 +21,105 @@ export class HomePage {
   public txtTimeNow: string;
   public txtDayNow: string;
   public inputVal: string = "variabel";
-  public txtTimeArrived: string = "07:48 AM";
-  public txtTimeBack: string = "-";
+  public txtTimeArrived: string = "";
+  public txtTimeBack: string = "";
   public txtWorkStatus: string = "Working";
-  geoAccuracy:number;
+  geoAccuracy: number;
 
-  constructor(public navCtrl: NavController, 
+  constructor(public navCtrl: NavController,
     public alertController: AlertController,
     public router: Router,
     private geolocation: Geolocation
-    ) { }
+  ) { }
 
   ngOnInit() {
-    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    var days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+    var dateData = this.GetDate();
 
-    var date = new Date();
-    var hr = date.getHours();
-    var ampm = "AM";
-    if (hr > 12) {
-      hr -= 12;
-      ampm = "PM";
-    }
-    var day = days[date.getDay()];
-    var tanggal = date.getDate();
-    var month = months[date.getMonth()];
-    var year = date.getFullYear();
-
-    var minute = date.getMinutes();
-    var minuteString = minute.toString();
-    if (minute < 10){
-      minuteString = "0" + minute;
-    }
-    this.txtDayNow = day + ", " + tanggal + " " + month + " " + year;
-    this.txtTimeNow = date.getHours() + ":" + minuteString + " " + ampm;
-
-    this.geolocation.getCurrentPosition().then((resp) => {
-      this.geoLatitude = 11;
-      this.geoLongitude = 0; 
-      this.geoAccuracy = resp.coords.accuracy; 
-      // this.getGeoencoder(this.geoLatitude,this.geoLongitude);
-     }).catch((error) => {
-     });
+    this.txtDayNow = dateData.day + ", " + dateData.date + " " + dateData.month + " " + dateData.year;
+    this.txtTimeNow = dateData.hrString + ":" + dateData.minuteString + " " + dateData.ampm;
   }
 
-  buttonAbsen() {
-    var today = new Date();
-    var time = today.getHours() + ":" + today.getMinutes();
-    var hours= today.getHours();
+  private GetDate(): DateData {
+    var dateData = new DateData();
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+    var date = new Date();
+
+    dateData.day = days[date.getDay()];
+    dateData.date = date.getDate();
+    dateData.month = months[date.getMonth()];
+    dateData.year = date.getFullYear();
+    dateData.minute = date.getMinutes();
+    dateData.minuteString = dateData.minute.toString();
+    if (dateData.minute < 10) {
+      dateData.minuteString = "0" + dateData.minute;
+    }
+    dateData.hr = date.getHours();
+    var hr = dateData.hr;
+    dateData.ampm = "AM";
+    if (dateData.hr > 12) {
+      hr -= 12;
+      dateData.ampm = "PM";
+    }
+    dateData.hrString = hr.toString();
+    if (hr < 10) {
+      dateData.hrString = "0" + hr;
+    }
+
+    return dateData;
+  }
+
+  async buttonAbsen() {
+    var dateData = this.GetDate();
+
+    if (!this.txtTimeArrived) {
+      this.txtTimeArrived = dateData.hrString + ":" + dateData.minuteString + " " + dateData.ampm;
+    } else if (!this.txtTimeBack) {
+      this.txtTimeBack = dateData.hrString + ":" + dateData.minuteString + " " + dateData.ampm;
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Alert',
+        subHeader: 'Subtitle',
+        message: 'This is an alert message.',
+        cssClass: 'alertcss',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+    }
+
     var options = {
       enableHighAccuracy: true,
       timeout: 5000,
       maximumAge: 0
     };
-    this.waktu = time;
 
-    
-  
+
+
     this.geolocation.getCurrentPosition(options).then((position) => {
       this.geoLatitude = position.coords.latitude;
-      this.geoLongitude = position.coords.longitude; 
-      this.geoAccuracy = position.coords.accuracy; 
+      this.geoLongitude = position.coords.longitude;
+      this.geoAccuracy = position.coords.accuracy;
       // this.getGeoencoder(this.geoLatitude,this.geoLongitude);
-     }).catch((error) => {
-       alert('GPS ANDA BELUM AKTIF');
-     });
+    }).catch((error) => {
+      alert('GPS ANDA BELUM AKTIF');
+    });
 
-     if(this.geoLatitude=0){
-       this.geoLatitude = 0;
-       this.geoLongitude = 11;
-     }
-    
-     if( this.geoLatitude <= -6.24508 && this.geoLatitude >= -6.24587 && this.geoLongitude >= 106.87269 && this.geoLongitude <= 106.87379 ){
-      if (hours >=6 && hours <=17 ) {
+    if (this.geoLatitude = 0) {
+      this.geoLatitude = 0;
+      this.geoLongitude = 11;
+    }
+
+    var hours = dateData.hr;
+    if (this.geoLatitude <= -6.24508 && this.geoLatitude >= -6.24587 && this.geoLongitude >= 106.87269 && this.geoLongitude <= 106.87379) {
+      if (hours >= 6 && hours <= 17) {
         alert("ABSEN DATANG DITERIMA");
-      } else if (hours > 17){ 
+      } else if (hours > 17) {
         alert("ABSEN PULANG");
       }
-     }else{
+    } else {
       alert("DILUAR LOKASI ABSEN");
-     }
+    }
 
   }
 
@@ -111,7 +132,7 @@ export class HomePage {
   //   }, 2000);
   // }
 
-  clickedButton(){
+  clickedButton() {
     this.router.navigate(['notifications'])
   }
 
@@ -150,4 +171,18 @@ export class HomePage {
 
     await alert.present();
   }
+}
+
+class DateData {
+  public day: string;
+  public date: number;
+  public month: string;
+  public year: number;
+  public hr: number;
+  public hrString: string;
+  public minute: number;
+  public minuteString: string;
+  public ampm: string;
+
+  constructor() { }
 }
