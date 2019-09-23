bb84af8a8172d1a298a5bfb4031d25a11d2e75f8
Nur Ridho A <abdurrahmansyah234@gmail.com>
2019-09-23 19:08:19 +0700

    - Add component report daily - Modify report page (auto set tanggal dalam sebulan)

---
+++ src/app/components/report-daily/report-daily.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-daily',
+  templateUrl: './report-daily.component.html',
+  styleUrls: ['./report-daily.component.scss'],
+})
+export class ReportDailyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
